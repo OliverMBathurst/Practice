14d643648c5859449ba60d959cc2290b2fa2f7f7
OliverMBathurst <oliverbathurst@outlook.com>
2022-01-07 17:51:30 +0000

    FIBO

---
+++ Rosalind/Algorithmic Heights/FIBO/Solution.cs
@@ -0,0 +1,22 @@
+static long FibNumAtIndex(int index) 
+{
+    if (index == 0)
+        return 0;
+
+    if (index == 1 || index == 2)
+        return 1;
+
+    int beforeNMinusOne = 1, beforeNMinusTwo = 1, current = 0;
+
+    for (var idx = 3; idx < index + 1; idx++)
+    {
+        current = beforeNMinusOne + beforeNMinusTwo;
+        beforeNMinusTwo = beforeNMinusOne;
+        beforeNMinusOne = current;
+    }
+
+    return current;
+}
+
+Console.WriteLine(FibNumAtIndex(20));
+//6765
