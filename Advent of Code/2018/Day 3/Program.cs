9bba54f4c9549399478ad9bf18356a76f27678e4
OliverMBathurst <oliverbathurst@outlook.com>
2023-02-21 17:33:52 +0000

    Added solutions

---
+++ Advent of Code/2018/Day 3/Program.cs
@@ -0,0 +1,31 @@
+﻿var fabricDefinitions = File.ReadAllLines(@"C:\Users\Oliver\Desktop\input.txt").Select(line =>
+{
+    var split = line.Split(" @ ");
+    var rhs = split[1].Split(": ");
+    string[] rhslhs = rhs[0].Split(','), rhsrhs = rhs[1].Split('x');
+    int fromLeftEdge = int.Parse(rhslhs[0]), width = int.Parse(rhsrhs[0]), fromTopEdge = int.Parse(rhslhs[1]), height = int.Parse(rhsrhs[1]);
+    return new
+    {
+        Id = split[0][1..],
+        Coordinates = Enumerable.Range(fromLeftEdge, width).SelectMany(w => Enumerable.Range(fromTopEdge, height).Select(h => (w, h)))
+    };
+}).ToArray();
+
+ var dict = new Dictionary<(int X, int Y), int>();
+foreach (var fabricDefinition in fabricDefinitions)
+{
+    foreach (var (X, Y) in fabricDefinition.Coordinates)
+    {
+        if (dict.ContainsKey((X, Y)))
+        {
+            dict[(X, Y)]++;
+        }
+        else
+        {
+            dict.Add((X, Y), 1);
+        }
+    }
+}
+
+Console.WriteLine(dict.Count(kvp => kvp.Value >= 2)); //117948
+Console.WriteLine(fabricDefinitions.First(fd => fd.Coordinates.All(c => dict[c] == 1)).Id); //567
---
+++ Advent of Code/2018/Day 4/Program.cs
@@ -0,0 +1,73 @@
+﻿var times = File.ReadAllLines(@"C:\Users\Oliver\Desktop\input.txt");
+
+static DateTime GetDateTime(string toParse)
+{
+    var stripped = toParse[1..].Split(" ");
+    string[] rhs = stripped[1].Split(':'), lhs = stripped[0].Split('-');
+    return new DateTime(year: int.Parse(lhs[0]), month: int.Parse(lhs[1]), day: int.Parse(lhs[2]), hour: int.Parse(rhs[0]), minute: int.Parse(rhs[1]), second: 0, millisecond: 0);
+}
+
+var startingShiftTimes = times
+    .Select(gd =>
+    {
+        var sp = gd.Split("] ");
+        return (Time: GetDateTime(sp[0]), Id: sp[1].Split(" ")[1]);
+    })
+    .OrderBy(x => x.Time)
+    .ToArray();
+
+var timeRecords = new Dictionary<DateTime, List<(DateTime Start, DateTime End, string GuardId)>>();
+
+var guardId = string.Empty;
+DateTime? start = null;
+
+foreach (var (Time, Id) in startingShiftTimes)
+{
+    switch (Id[0])
+    {
+        case '#':
+            guardId = Id;
+            break;
+        case 'a':
+            start = Time;
+            break;
+        case 'u':
+            var end = Time;
+            if (start != null)
+            {
+                var day = new DateTime(year: Time.Year, month: Time.Month, day: Time.Day);
+                if (timeRecords.ContainsKey(day))
+                    timeRecords[day].Add((start.Value, end, guardId));
+                else
+                    timeRecords.Add(day, new List<(DateTime Start, DateTime End, string GuardId)> { (start.Value, end, guardId) });
+            }
+            break;
+    }
+}
+
+var timestamps = timeRecords.Values.SelectMany(x => x).GroupBy(x => x.GuardId).ToArray();
+var mostAsleep = timestamps.Select(g => new { Id = g.Key, Value = g.Sum(x => (x.End - x.Start).Minutes) }).MaxBy(x => x.Value).Id;
+
+var hours = new Dictionary<string, Dictionary<(int Hour, int Minute), int>>();
+foreach (var grouping in timestamps)
+{
+    var id = grouping.Key;
+    hours.Add(id, new Dictionary<(int Hour, int Minute), int>());
+    foreach (var (Start, End, _) in grouping)
+    {
+        var total = (End - Start).Minutes;
+        for (var i = 0; i < total; i++)
+        {
+            var addedMinutes = Start.AddMinutes(i);
+            var hoursMinutes = (addedMinutes.Hour, addedMinutes.Minute);
+            if (hours[id].ContainsKey(hoursMinutes))
+                hours[id][hoursMinutes]++;
+            else
+                hours[id].Add(hoursMinutes, 1);
+        }
+    }
+}
+
+Console.WriteLine(hours[mostAsleep].MaxBy(x => x.Value).Key.Minute * int.Parse(mostAsleep[1..])); //19025
+var guardWithMaximum = hours.MaxBy(kvp => kvp.Value.Max(values => values.Value));
+Console.WriteLine(int.Parse(guardWithMaximum.Key[1..]) * guardWithMaximum.Value.MaxBy(c => c.Value).Key.Minute); //23776
